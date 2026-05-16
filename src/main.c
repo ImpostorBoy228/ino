0d41682d647ec9376f00558c35d275d4ffc5adce
ImpostorBoy <impostorboy@yandex.ru>
2026-05-16 20:56:12 +0700

    add unistd.h

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void printData(unsigned char* data, long dataSize) {
     for (long i = 0; i < (dataSize); i++) {
